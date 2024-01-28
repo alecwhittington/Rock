29b88607e6461b96f28eeca30496e2b29607df4e
Daniel Hazelbaker <daniel@blueboxmoon.com>
2024-01-27 17:57:33 -0700

    - Added support for tests to use an isolated database instance.

---
+++ Rock.Tests.Shared/TestFramework/DatabaseContainerImageBuilder.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Docker.DotNet;
+using Docker.DotNet.Models;
+
+using DotNet.Testcontainers.Containers;
+
+using Rock.Utility.Settings;
+
+using Testcontainers.MsSql;
+
+namespace Rock.Tests.Shared.TestFramework
+{
+    /// <summary>
+    /// Builds a new Docker image that has the required database information
+    /// which can be later used for fast test running.
+    /// </summary>
+    public class DatabaseContainerImageBuilder
+    {
+        public const string RepositoryName = "rockrms/tests-integration";
+
+        /// <summary>
+        /// Builds a new image for the current migration target.
+        /// </summary>
+        /// <returns>A task that indicates when the operation has completed.</returns>
+        public async Task BuildAsync()
+        {
+            using ( var dockerClient = new DockerClientConfiguration().CreateClient() )
+            {
+                var container = new MsSqlBuilder()
+                    .Build();
+
+                await container.StartAsync();
+
+                await BuildContainerAsync( container );
+
+                await container.StopAsync();
+
+                await dockerClient.Images.CommitContainerChangesAsync( new CommitContainerChangesParameters
+                {
+                    ContainerID = container.Id,
+                    RepositoryName = RepositoryName,
+                    Tag = GetTargetMigration(),
+                    Changes = new List<string>
+                    {
+                        $"LABEL {ResourceReaper.ResourceReaperSessionLabel}="
+                    }
+                } );
+            }
+        }
+
+        /// <summary>
+        /// Builds the container so it contains the required information.
+        /// </summary>
+        /// <param name="container">The container to be built.</param>
+        /// <returns>A task that indicates when the operation has completed.</returns>
+        private static async Task BuildContainerAsync( MsSqlContainer container )
+        {
+            var connectionString = container.GetConnectionString();
+
+            using ( var connection = new SqlConnection( connectionString ) )
+            {
+                await connection.OpenAsync();
+
+                await CreateDatabaseAsync( connection, "Rock" );
+
+                var csb = new SqlConnectionStringBuilder( connectionString );
+                csb.InitialCatalog = "Rock";
+
+                RockInstanceConfig.Database.SetConnectionString( csb.ConnectionString );
+                RockInstanceConfig.SetDatabaseIsAvailable( true );
+
+                MigrateDatabase( csb.ConnectionString );
+
+                RockInstanceConfig.SetDatabaseIsAvailable( false );
+                RockInstanceConfig.Database.SetConnectionString( string.Empty );
+            }
+        }
+
+        /// <summary>
+        /// Creates the named database.
+        /// </summary>
+        /// <param name="connection">The connection to execute the command on.</param>
+        /// <param name="dbName">The name of the database to create.</param>
+        /// <returns>A task that indicates when the operation has completed.</returns>
+        private static async Task CreateDatabaseAsync( SqlConnection connection, string dbName )
+        {
+            LogHelper.Log( $"Creating new database..." );
+
+            using ( var cmd = connection.CreateCommand() )
+            {
+                cmd.CommandText = $@"
+CREATE DATABASE [{dbName}];
+ALTER DATABASE [{dbName}] SET RECOVERY SIMPLE";
+
+                await cmd.ExecuteNonQueryAsync();
+            }
+        }
+
+        /// <summary>
+        /// Migrates the database.
+        /// </summary>
+        private static void MigrateDatabase( string connectionString )
+        {
+            var connection = new DbConnectionInfo( connectionString, "System.Data.SqlClient" );
+
+            var config = new Rock.Migrations.Configuration
+            {
+                TargetDatabase = connection
+            };
+
+            var migrator = new System.Data.Entity.Migrations.DbMigrator( config );
+
+            try
+            {
+                migrator.Update();
+            }
+            catch ( Exception ex )
+            {
+                throw new Exception( "Test Database migration failed. Verify that the database connection string specified in the test project is valid. You may need to manually synchronize the database or configure the test environment to force-create a new database.", ex );
+            }
+        }
+
+        /// <summary>
+        /// Gets the target migration.
+        /// </summary>
+        private static string GetTargetMigration()
+        {
+            return typeof( Rock.Migrations.RockMigration )
+                .Assembly
+                .GetExportedTypes()
+                .Where( a => typeof( System.Data.Entity.Migrations.Infrastructure.IMigrationMetadata ).IsAssignableFrom( a ) )
+                .Select( a => ( System.Data.Entity.Migrations.Infrastructure.IMigrationMetadata ) Activator.CreateInstance( a ) )
+                .Select( a => a.Id.Substring( 0, 15 ) )
+                .OrderByDescending( a => a )
+                .First();
+        }
+
+        /// <summary>
+        /// Gets the repository name and tag for the image that represents
+        /// the current migration.
+        /// </summary>
+        /// <returns></returns>
+        public static string GetRepositoryAndTag()
+        {
+            return $"{RepositoryName}:{GetTargetMigration()}";
+        }
+    }
+}
---
+++ Rock.Tests.Shared/TestFramework/DatabaseTestsBase.cs
@@ -0,0 +1,127 @@
+﻿using System.Reflection;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Rock.Tests.Shared.TestFramework
+{
+    /// <summary>
+    /// All unit tests that require database access should inherit from
+    /// this class. It will abstract how away how a database is provided
+    /// to each test. Currently this is done with docker images.
+    /// </summary>
+    public abstract class DatabaseTestsBase
+    {
+        /// <summary>
+        /// <c>true</c> if we need to destroy the test container after the
+        /// current test has finished.
+        /// </summary>
+        private bool _testWantsIsolatedDatabase = false;
+
+        /// <summary>
+        /// The current container providing the database.
+        /// </summary>
+        private static TestDatabaseContainer _container;
+
+        /// <summary>
+        /// Set by the test framework to indicate which test is running.
+        /// </summary>
+        public TestContext TestContext { get; set; }
+
+        #region Methods
+
+        /// <summary>
+        /// Called before any test is executed in the entire class.
+        /// </summary>
+        /// <param name="context">The context of the first test that will be run.</param>
+        /// <returns>A task that indicates when the operation has completed.</returns>
+        [ClassInitialize( InheritanceBehavior.BeforeEachDerivedClass )]
+        public static Task ContainerClassInitialize( TestContext context )
+        {
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Called after every test in the class has finished executing.
+        /// </summary>
+        /// <returns>A task that indicates when the cleanup has completed.</returns>
+        [ClassCleanup( InheritanceBehavior.BeforeEachDerivedClass )]
+        public static async Task ContainerClassCleanup()
+        {
+            // Make sure we shut down the container at the end of all tests
+            // in this class.
+            if ( _container != null )
+            {
+                try
+                {
+                    await _container.DisposeAsync();
+                }
+                finally
+                {
+                    _container = null;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Called before each test in the class is executed. This will
+        /// also be called before each data row of the test is executed.
+        /// So if a test has 20 data rows and requests a new database, we
+        /// will spin up a new database 20 times.
+        /// </summary>
+        /// <returns>A task that indicates when the initialization has completed.</returns>
+        [TestInitialize]
+        public async Task ContainerTestInitialize()
+        {
+            var method = GetType().GetMethod( TestContext.TestName );
+
+            _testWantsIsolatedDatabase = method.GetCustomAttribute<IsolatedTestDatabaseAttribute>() != null
+                || GetType().GetCustomAttribute<IsolatedTestDatabaseAttribute>() != null;
+
+            if ( _container == null || _testWantsIsolatedDatabase )
+            {
+                if ( _container != null )
+                {
+                    try
+                    {
+                        await _container.DisposeAsync();
+                    }
+                    finally
+                    {
+                        _container = null;
+                    }
+                }
+
+                var container = new TestDatabaseContainer();
+
+                await container.StartAsync();
+
+                _container = container;
+            }
+        }
+
+        /// <summary>
+        /// Called after each test in the class has completed.
+        /// </summary>
+        /// <returns>A task that indicates when cleanup is completed.</returns>
+        [TestCleanup]
+        public async Task ContainerTestCleanup()
+        {
+            // If the test indicated that it needed an isolated database
+            // then shut it down so the next test gets a fresh one.
+            if ( _testWantsIsolatedDatabase && _container != null )
+            {
+                try
+                {
+                    await _container.DisposeAsync();
+                }
+                finally
+                {
+                    _container = null;
+                }
+            }
+        }
+
+        #endregion
+    }
+}
